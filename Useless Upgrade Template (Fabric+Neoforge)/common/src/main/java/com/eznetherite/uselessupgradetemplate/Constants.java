11cb7363c8a68b25dc3b0d87fb2038e423454ec4
break08 <dungluong3108@gmail.com>
2026-03-29 15:30:04 +0700

    26.1 Pull

---
+++ Useless Upgrade Template (Fabric+Neoforge)/common/src/main/java/com/eznetherite/uselessupgradetemplate/Constants.java
@@ -0,0 +1,11 @@
+package com.eznetherite.uselessupgradetemplate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "useless_upgrade_template";
+	public static final String MOD_NAME = "UselessUpgradeTemplate";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
